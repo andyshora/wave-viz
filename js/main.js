6deeca49531feb1213314f056cf938128ead6879
Andy Shora <andyshora@gmail.com>
2015-02-21 17:10:30 +0000

    Sparks

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-var gridSize = 12; // will create gridSize ^ 2 points
-var pointSize = 50;
-var refreshInterval = 60;
-var energyPropagationPerFrame = .8;
-var energyLostPerFrame = 5;
+var gridSize = 100; // will create gridSize ^ 2 points
 
+var gridWidth = 80;
+var gridHeight = 80;
+
+var pointSize = 10;
+var energyPropagationPerFrame = .4;
+var energyLostPerFrame = 10;
 var pointMargin = 1;
+
 var points = [];
 var energyTransferQueue = [];
 var t0 = +new Date();
 
 // RAF
 var start = null;
-var fps = 30;
+var fps = 60;
 
 var canvas = document.getElementById('canvas');
-canvas.width = canvas.height = (gridSize * pointSize) + (gridSize-1 * pointMargin);
+canvas.width = (gridWidth * pointSize) + (gridWidth-1 * pointMargin);
+canvas.height = (gridHeight * pointSize) + (gridHeight-1 * pointMargin);
 var context = canvas.getContext('2d');
 
 
@@ -24,6 +28,7 @@ var Point = function(x, y) {
     x: x,
     y: y,
     increased: 0,
+    requiresRender: false,
     energy: 0, // 0-100
     addEnergy: function(energy) {
 
@@ -35,14 +40,18 @@ var Point = function(x, y) {
         this.increased = 0;
       }
 
+      // only add energy if point doesnt already have any
       var tempEnergy = this.energy + energy;
+
       if (tempEnergy > 100) {
         tempEnergy = 100;
       } else if (tempEnergy < 0) {
         tempEnergy = 0;
       }
-      this.energy = tempEnergy;
-      // console.log('this.energy', this.energy);
+
+      this.energy = tempEnergy < 10 ? 0 : tempEnergy;
+      this.requiresRender = true;
+
     },
     sendEnergy: function() {
 
@@ -60,13 +69,14 @@ var Point = function(x, y) {
 
       for (var i = 0; i < neighbours.length; i++) {
         var n = neighbours[i];
-        if ((n.x >= 0) && (n.x < gridSize) && (n.y >= 0) && (n.y < gridSize)) {
+        if ((n.x >= 0) && (n.x < gridWidth) && (n.y >= 0) && (n.y < gridHeight)) {
           var index = getPointIndex(n.x, n.y);
           // add a fraction of this points energy
           // to neighbouring points
           // console.log('index', index, points);
           var obj = { index: index, energy: Math.round(this.energy * energyPropagationPerFrame) };
-          if (points[index].energy < Math.round(this.energy * energyPropagationPerFrame)) {
+          // if (points[index].energy < Math.round(this.energy * energyPropagationPerFrame)) {
+          if (!points[index].energy) {  
             energyTransferQueue.push(obj);
           }
         }
@@ -75,20 +85,20 @@ var Point = function(x, y) {
     },
     render: function(t) {
 
-
+      this.requiresRender = false;
 
       context.beginPath();
 
       var depressedPerc = 1 - (this.energy / 100); // todo - this should be based on sin(t)
       var customPointpointSize = pointSize * depressedPerc;
 
-      if (customPointpointSize < pointSize / 2) {
-        customPointpointSize = pointSize / 2;
+      if (customPointpointSize < pointSize * .8) {
+        customPointpointSize = pointSize * .8;
       }
       var extraMargin = (pointSize - customPointpointSize) / 2;
 
-      // context.fillStyle = 'rgb(255,' + Math.floor(255 - this.energy) + ',' + Math.floor(255 - this.energy) + ')';
-      context.fillStyle = 'red';
+      context.fillStyle = getColor(this.energy);
+      // context.fillStyle = 'rgb(255,0,0)';
       context.fillRect((x * pointSize) + (x * pointMargin) + extraMargin, (y * pointSize) + (y * pointMargin) + extraMargin, customPointpointSize, customPointpointSize);
 
       if (!this.energy) {
@@ -116,8 +126,8 @@ var Point = function(x, y) {
 
 
 
-for (var y = 0; y < gridSize; y++) {
-  for (var x = 0; x < gridSize; x++) {
+for (var y = 0; y < gridHeight; y++) {
+  for (var x = 0; x < gridWidth; x++) {
     var index = points.length;
     points.push(new Point(x, y));
     points[index].render(t0);
@@ -143,22 +153,24 @@ function updatePoints(progress) {
 
   clearCanvas();
 
+  
+
   // update points
   transferScheduledEnergy();
 
-  // var t = +new Date();
-
-  for (var y = 0; y < gridSize; y++) {
-    for (var x = 0; x < gridSize; x++) {
+  // todo - speed this up
+  for (var y = 0; y < gridHeight; y++) {
+    for (var x = 0; x < gridWidth; x++) {
       var index = getPointIndex(x, y);
       points[index].render(progress);
+      
     }
   }
 
 }
 
 function getPointIndex(x, y) {
-  return (y * gridSize) + x;
+  return (y * gridHeight) + x;
 }
 
 function onUpdateClicked() {
@@ -168,22 +180,64 @@ function onUpdateClicked() {
 
 // start FAF loop
 
+var a = false;
+
 function step(timestamp) {
 
   if (!start) {
     start = +new Date();
   }
 
-  setTimeout(function() {
+  var progress = timestamp - start;
 
-    var progress = timestamp - start;
+  if (a) {
     updatePoints(progress);
+    
+  }
 
-    if (progress < 5000) {
-      window.requestAnimationFrame(step);
-    }
-  }, 1000 / fps);
+
+
+  a = !a;
+  window.requestAnimationFrame(step);
+ 
+
+
+
 }
+
+function getTapPosition(event) {
+  // console.log('getTapPosition');
+  var x = event.x;
+  var y = event.y;
+
+  x -= canvas.offsetLeft;
+  y -= canvas.offsetTop;
+
+  var index = getPointTapped(x, y);
+  energyTransferQueue.push({ index: index, energy: 100 });
+}
+
+function getPointTapped(x, y) {
+  var x2 = Math.floor(x / (pointSize + pointMargin));
+  var y2 = Math.floor(y / (pointSize + pointMargin));
+
+  return getPointIndex(x2, y2);
+}
+
+function getColor(energy) {
+
+  var spark = Math.random() > .9;
+
+  var r = Math.floor(energy * 2.55);
+  var g = spark ? Math.floor(energy * 2.55) : 0;
+  var b = spark ? Math.floor(energy * 2.55) : 0;
+
+  return 'rgb(' + r + ',' + g + ',' + b + ')';
+
+}
+
+canvas.addEventListener('touchstart', getTapPosition, false);
+canvas.addEventListener('mousedown', getTapPosition, false);
 
 start = +new Date();
 window.requestAnimationFrame(step);
