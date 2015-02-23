72634afda8dee64d92d03006d367ea01f3d58366
Andy Shora <andyshora@gmail.com>
2015-02-23 22:24:04 +0000

    Events

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+var connectedCount = 0;
 
 // app settings
 app.set('port', process.env.PORT || 8081);
@@ -10,15 +11,23 @@ app.set('port', process.env.PORT || 8081);
 app.use('/', express.static(__dirname + '/static', { maxAge: 86400 }));
 
 io.on('connection', function (socket) {
-  socket.emit('client connected', {});
+  connectedCount++;
+  console.log('connectedCount', connectedCount);
+  socket.emit('room:count', { count: connectedCount });
 
+  // controller emitted signal which needs relaying to game screen
   socket.on('game:trigger-wave', function(data) {
-    io.emit('game:trigger-wave', data);
+    socket.emit('game:trigger-wave', data);
     console.log('game:trigger-wave');
   });
 
-});
+  socket.on('disconnect', function (socket) {
+    connectedCount--;
+    console.log('connectedCount', connectedCount);
+    socket.emit('room:count', { count: connectedCount });
+  });
 
+});
 
 
 server.listen(app.get('port'));
